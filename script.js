bb0db8474b95eb4e7f6ae17274f623be76a52ada
seu-username <seu-email>
2026-06-06 17:27:33 -0300

    Cores

--- script.js
+++ script.js
@@ -3,34 +3,32 @@ let currentInput = "";
 let currentOperation = "";
 
 function appendNumber(value) {
-    currentInput += value;
-    display.textContent = currentInput;
+  currentInput += value;
+  display.textContent = currentInput;
 }
 
 function appendOperation(operation) {
-    if (currentInput === "" && operation !== ".") return;
-    currentInput += operation;
-    display.textContent = currentInput;
+  if (currentInput === "" && operation !== ".") return;
+  currentInput += operation;
+  display.textContent = currentInput;
 }
 
 function calculate() {
-    try {
-        let result = eval(currentInput);
-        if (!Number.isInteger(result)) {
-            result = result.toFixed(2);
-        }
-        currentInput = result;
-        display.textContent = currentInput;
-    } catch (error) {
-        display.textContent = "Erro";
-        currentInput = "";
+  try {
+    let result = eval(currentInput);
+    if (!Number.isInteger(result)) {
+      result = result.toFixed(2);
     }
-
-
+    currentInput = result;
+    display.textContent = currentInput;
+  } catch (error) {
+    display.textContent = "Erro";
+    alert("Adicione um número");
+    currentInput = "";
+  }
 }
 
-
 function clearDisplay() {
-    currentInput = "";
-    display.textContent = currentInput;
-}  
+  currentInput = "";
+  display.textContent = currentInput;
+}
